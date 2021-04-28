b7beb22d803168a3bd54efcfa7152748606bd422
Sebastian Konicz <sebastian.konicz@gmail.com>
2021-04-28 22:51:05 +0200

    Adjusted photo to IFrame

--- src/01_mural_map.py
+++ src/01_mural_map.py
@@ -39,7 +39,7 @@ def main():
         photo_path = project_dir + r'\data\murale_img\{photo_path}.jpg'.format(photo_path=photo)
         # marker setting
         encoded = base64.b64encode(open(photo_path, 'rb').read())
-        html = '<img src="data:image/png;base64,{}">'.format
+        html = '<img style="width:100%; height:100%;" src="data:image/png;base64,{}">'.format
         iframe = IFrame(html(encoded.decode('UTF-8')), width=400, height=350)
         popup = folium.Popup(iframe, max_width=800)
 
@@ -47,41 +47,6 @@ def main():
                       tooltip=html, popup=popup,
                       # popup='<img src={path}>'.format(path=photo_path),
                       icon=folium.Icon(color='blue', icon='camera', prefix='fa')).add_to(map_graph)
-
-
-    # choropleth = folium.Choropleth(geo_data=map_geo,
-    #                   name='choropleth',
-    #                   data=data,
-    #                   columns=['teryt', 'unempl_%'],
-    #                   key_on='feature.properties.JPT_KOD_JE',
-    #                   fill_color='YlOrRd',
-    #                   fill_opacity=0.7,
-    #                   line_opacity=0.2,
-    #                   legend_name="Stopa bezrobocia w procentach"
-    #                                ).add_to(map_graph)
-
-    # choropleth.geojson.add_child(folium.features.GeoJsonTooltip(['pow_name'], labels=False))
-
-    # # adding labels to map
-    # style_function = lambda x: {'fillColor': '#ffffff',
-    #                             'color': '#000000',
-    #                             'fillOpacity': 0.1,
-    #                             'weight': 0.1}
-    #
-    # tooltip = folium.features.GeoJson(
-    #     map_geo,
-    #     style_function=style_function,
-    #     control=False,
-    #     tooltip=folium.features.GeoJsonTooltip(
-    #         fields=['pow_name', 'unempl_%'],
-    #         aliases=['nazwa', 'stopa bezrobocia (%)'],
-    #         style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;")
-    #     )
-    # )
-    # map_graph.add_child(tooltip)
-    # map_graph.keep_in_front(tooltip)
-    # folium.LayerControl().add_to(map_graph)
-
     # saving map
     print('saving map')
     map_graph.save(project_dir + r'\data\final\mural_map.html')
